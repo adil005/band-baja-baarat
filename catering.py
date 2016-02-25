2a3c81d44c85bee246b1a1f266d412dc87317848
adil005 <irfanadil121@gmail.com>
2016-02-26 00:17:05 +0530

    added the database part

--- catering.py
+++ catering.py
@@ -1,19 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
 
-def caters(city): #web scraping is done to fetch data for caters using beautiful soup
+def caters(city,locality): #web scraping is done to fetch data for caters using beautiful soup
+    conn=sqlite3.connect('adi.db')
+    cur=conn.cursor()
     url='http://yellowpages.sulekha.com/catering-services_'+city
     r=requests.get(url)
     s=BeautifulSoup(r.content)
     g=s.find_all("li",{"itemtype":"http://schema.org/LocalBusiness"})
     for item in g:
         try:
-            print item.find_all("a",{"class":"YPTRACK GAQ_C_BUSL"})[0].text
-            print item.find_all("b",{"class":"contact-number"})[0].text
+            a=item.find_all("a",{"class":"YPTRACK GAQ_C_BUSL"})[0].text
+            b=item.find_all("b",{"class":"contact-number"})[0].text
         except:
             pass
         try:
-            print item.find_all("address",{"class":"pull-left"})[0].text
+            c=item.find_all("address",{"class":"pull-left"})[0].text
         except:
             pass
-        print "\n"
+        cur.execute('INSERT INTO Mycaters(city,cater_name,locality,contact_cater,address_cater) VALUES(?,?,?,?,?)',(city,a,locality,b,c))
+        conn.commit()
+    conn.close()    
